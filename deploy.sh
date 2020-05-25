a9b5c62750acc4c99b8d35938ae10ed87e79281c
Nowell <nclosser@wanderu.com>
2020-05-24 22:57:46 -0700

    change crontab name

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,13 @@ set -e
 git push origin master
 
 ssh -T ec2-user@52.39.88.38 << EOF
+set -e
 cd studybuddy
 git pull origin master
 ls
-mv crontab /var/spool/cron/ec2-user
+> /var/spool/cron/ec2-user
+echo TRELLO_TOKEN=$TRELLO_TOKEN >> /var/spool/cron/ec2-user
+cat crontab-ec2-user >> /var/spool/cron/ec2-user
+0bf792b2f82819cf04cd535c7205cb6a145bf58b
 logout
 EOF
