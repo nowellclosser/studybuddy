aca909259ca3bda7b525e406ae689f5ae306e4f8
Nowell <nclosser@wanderu.com>
2021-10-31 21:27:04 -0700

    reorganize paths

--- webapp/app.py
+++ webapp/app.py
@@ -12,13 +12,29 @@ def home():
 def fake_page():
     return bottle.static_file('fake_page.html', root='/home/ec2-user/studybuddy/webapp/static')
 
-@bottle.get(r'<:re:.*/><filename:re:.*\.js>')
-def js(filepath):
-    return static_file(filepath, root='/home/ec2-user/studybuddy/webapp/static')
+# @bottle.get(r'<:re:.*/><filename:re:.*\.js>')
+# def js(filepath):
+#     return static_file(filepath, root='/home/ec2-user/studybuddy/webapp/static')
 
-@bottle.get(r'<:re:.*/><filename:re:.*\.css>')
+# @bottle.get(r'<:re:.*/><filename:re:.*\.css>')
+# def js(filepath):
+#     return static_file(filepath, root='/home/ec2-user/studybuddy/webapp/static')
+
+@bottle.get("/static/js-v3/<filepath:re:.*\.js>")
+def jsv3(filepath):
+    return static_file(filepath, root="static/js-v3")
+
+@bottle.get("/static/css-v3/<filepath:re:.*\.css>")
+def cssv3(filepath):
+    return static_file(filepath, root="static/css-v3")
+
+@bottle.get("/static/js/<filepath:re:.*\.js>")
 def js(filepath):
-    return static_file(filepath, root='/home/ec2-user/studybuddy/webapp/static')
+    return static_file(filepath, root="static/js")
+
+@bottle.get("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="static/css")
 
 # For Trello to validate our callback URL
 @bottle.get('/process_card_update/<card_type>/<card_id>')
